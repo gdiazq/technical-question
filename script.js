5d6c10755d0e7afee2a579db355e667cc6211a98
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-08-22 01:08:54 -0400

    En la prueba me puse nervioso, que lastima

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ const lower_case='abcdefghijklmnopqrstuvwxyz'
 const super_case='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const special_characters='!@#$%^&*()-+'
 
-const password = "Kefaf#2"
+const password = "Ke#2"
 
 function minimunNumber(password) {
-    for (let i = 0; i < password.length; i++) {
-        if (numbers.includes(password[i])) {
-            console.log("El password contiene un número")
+    if (password.length < 6) {
+        console.log('El password es muy corto')
+    } else {
+        for (let i = 0; i < password.length; i++) {
+            if (numbers.includes(password[i])) {
+                console.log('El password contine un numero')
+            } 
+            if (lower_case.includes(password[i])) {
+                console.log('El password contine una letra minuscula')
+            }
+            if (super_case.includes(password[i])) {
+                console.log('El password contine una letra mayuscula')
+            }
+            if (special_characters.includes(password[i])) {
+                console.log('El password contine un caracter especial')
+            }
         }
     }
 }
