b4c5bce7568cb95a135306448ac576e9d8364d78
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-08-22 02:15:11 -0400

    Esta listo

--- script.js
+++ script.js
@@ -3,27 +3,47 @@ const lower_case='abcdefghijklmnopqrstuvwxyz'
 const super_case='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const special_characters='!@#$%^&*()-+'
 
-const password = "Ke#2"
+const password = "a#addawdwadawdwad77"
 
 function minimunNumber(password) {
-    if (password.length < 6) {
-        console.log('El password es muy corto')
-    } else {
-        for (let i = 0; i < password.length; i++) {
-            if (numbers.includes(password[i])) {
-                console.log('El password contine un numero')
-            } 
-            if (lower_case.includes(password[i])) {
-                console.log('El password contine una letra minuscula')
-            }
-            if (super_case.includes(password[i])) {
-                console.log('El password contine una letra mayuscula')
-            }
-            if (special_characters.includes(password[i])) {
-                console.log('El password contine un caracter especial')
-            }
+    let has_numbers = false
+    let has_lower = false
+    let has_super = false
+    let has_special = false
+    let total_missing_criteria = 0
+
+    for (let i = 0; i < password.length; i++) {
+        if (numbers.includes(password[i])) {
+            has_numbers = true
+        } 
+        if (lower_case.includes(password[i])) {
+            has_lower = true
+        }
+        if (super_case.includes(password[i])) {
+            has_super = true
+        }
+        if (special_characters.includes(password[i])) {
+            has_special = true
         }
     }
+
+    if (!has_numbers) {
+        total_missing_criteria++
+    }
+    if (!has_lower) {
+        total_missing_criteria++
+    }
+    if (!has_super) {
+        total_missing_criteria++
+    }
+    if (!has_special) {
+        total_missing_criteria++
+    }
+    const lengthDeficit = Math.max(0, 6 - password.length);
+    
+    const charactersToAdd = Math.max(total_missing_criteria, lengthDeficit);
+
+    return charactersToAdd;
 }
 
 minimunNumber(password)
