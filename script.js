ae966e699ce3bc3f17bc4c6fb321617112b4cb6e
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-08-21 22:07:13 -0400

    first code js

---
+++ script.js
@@ -0,0 +1,8 @@
+const numbers='0123456789'
+const lower_case='abcdefghijklmnopqrstuvwxyz'
+const super_case='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const special_characters='!@#$%^&*()-+'
+
+function minimunNumber(password) {
+   // tu codigo
+}
