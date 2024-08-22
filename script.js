f1506357da8d2af5011a3f0172f9be6cc41e19b1
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-08-22 00:47:56 -0400

    Comenzando con el codigo

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const lower_case='abcdefghijklmnopqrstuvwxyz'
 const super_case='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const special_characters='!@#$%^&*()-+'
 
+const password = "Kefaf#2"
+
 function minimunNumber(password) {
-   // tu codigo
+    for (let i = 0; i < password.length; i++) {
+        if (numbers.includes(password[i])) {
+            console.log("El password contiene un número")
+        }
+    }
 }
+
+minimunNumber(password)
